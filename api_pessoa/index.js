1d6f4ce7877d18d9eb0e67bb4622242795ecf2aa
Lucascaetanoferreira <lucascaetanoferreira232930@gmail.com>
2025-09-04 21:51:00 -0300

    api_pessoa

--- api_pessoa/index.js
+++ api_pessoa/index.js
@@ -1,7 +1,7 @@
 import express, { response } from "express";
 
 const app = express();
-app.use(express.json());
+app.use(express.json()); //configura\\
 
 const pessoa =[];
 var coutID=0;
@@ -13,18 +13,50 @@ pessoa.push({
 
 app.get("/", (request,respose)=>{
 
+    const pessoa =pessoa.filter ((i)=> i.nome === nome);
+
     return respose.send(pessoa);
 
 });
+app.put ("/atualizar:id", (request,response)=>{
+   const {id} = request.params;
+   const {nome, telefone} = request.body;
 
-app.post("/cadastro", (request,response)=>{
+   const pessoaIndex = pessoa.findIndex((i)=> i.id == id);
+
+   if(pessoaIndex <= -1){
+    return response.send({msg:"O codigo da pessoa não existe"})
+
+   }
+
+   pessoa={pessoaIndex}. nome=nome;
+   pessoa={pessoaIndex}.telefone=telefone;
+});
+
+app.delete ("/deletar:id", (request,response)=>{
+    const {id} = request.params;
+    
+    const pessoaIndex = pessoa.findIndex((i)=> i.id == id);
+
+    if(pessoaIndex <=-1){
+        return response.send("msg: O codigo da pessoa esta invalida");
+
+    }else{
+        pessoa.splice(pessoaIndex,1);
+    }
+    return response.send("msg: pessoa deletada com sucesso");
+
+})
+
+
+app.post("/cadastro", (request,response)=>{//permite que envie um corpo//
 
     console.log(request);
 
     const nome = request.body.nome;
     const telefone = request.body.telefone;
 
-    pessoa.push({
+    pessoa.push({//objeto\\
         id: coutID++,
         nome,
         telefone
